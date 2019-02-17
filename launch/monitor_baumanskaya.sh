83f1fafd1b71f123004f15c8bd0836923317fa18
Yury.D <SoulGate@yandex.ru>
2019-02-17 16:56:14 +0300

    Fixed launch scripts (yes, already).

--- launch/monitor_baumanskaya.sh
+++ launch/monitor_baumanskaya.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../ytmonitor_wd.py \
+python3 ../ytm_wd \
 --verbose 4 \
 --url "https://yandex.ru/maps/213/moscow/?ll=37.678948%2C55.772062&masstransit%5BstopId%5D=stop__9643291&mode=stop&z=19" \
 --chrome_driver_location "/usr/sbin/chromedriver" \
--- launch/monitor_kitay-gorod.sh
+++ launch/monitor_kitay-gorod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../ytmonitor_wd.py \
+python3 ../ytm_wd \
 --verbose 4 \
 --url "https://yandex.ru/maps/213/moscow/?ll=37.634676%2C55.754150&masstransit%5BstopId%5D=stop__10187976&mode=stop&z=19" \
 --chrome_driver_location "/usr/sbin/chromedriver" \
