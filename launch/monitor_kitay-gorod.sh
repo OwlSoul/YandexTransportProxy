0fdb14b2b5d3f40e048f485fd72bd7e56a697f16
Yury D <SoulGate@yandex.ru>
2019-03-04 14:36:35 +0300

    Minor changes to one monitor example script.

--- launch/monitor_kitay-gorod.sh
+++ launch/monitor_kitay-gorod.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
 python3 ../ytm_wd \
---verbose 4 \
+--verbose 1 \
+--station_id "КИТАЙ ГОРОД" \
 --url "https://yandex.ru/maps/213/moscow/?ll=37.634676%2C55.754150&masstransit%5BstopId%5D=stop__10187976&mode=stop&z=19" \
 --chrome_driver_location "/usr/lib/chromium-browser/chromedriver" \
 --wait_time 60 \
 --run_once \
---out_mode "csv" \
---save_to_database \
---db_host "localhost" \
---db_port 5432 \
---db_name "ytmonitor" \
---db_username "ytmonitor" \
---db_password "password"
+--out_mode "plain" \
