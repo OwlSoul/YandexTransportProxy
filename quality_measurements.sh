9d91571c1a45a3a7edd8d56a6fb98eb4993909ec
Yury D <SoulGate@yandex.ru>
2019-03-13 23:59:45 +0300

    Testing Jenkins plot, it's non-intuitive.

--- quality_measurements.sh
+++ quality_measurements.sh
@@ -4,8 +4,13 @@
 
 # Linter command
 LINTER_COMMAND="pylint ytm_wd ytm_pageparser.py"
+CODE_QUALITY_FILE="code_quality.csv"
 
 # Linter results
 CODE_Q=$($LINTER_COMMAND | grep -oP "(?<=Your code has been rated at).*?(?=/)")
 echo "Code quality: $CODE_Q"
-echo $CODE_Q > code_quality.csv
+echo "\"Code Quality\"" > $CODE_QUALITY_FILE
+echo $CODE_Q > $CODE_QUALITY_FILE
+echo $CODE_Q > $CODE_QUALITY_FILE
+echo $CODE_Q > $CODE_QUALITY_FILE
+
