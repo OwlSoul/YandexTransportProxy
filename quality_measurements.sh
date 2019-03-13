7dbdf2cce9fbbc3036a0fec1d6bf2de18bb1fc59
Yury D <SoulGate@yandex.ru>
2019-03-14 00:03:05 +0300

    More tests

--- quality_measurements.sh
+++ quality_measurements.sh
@@ -9,8 +9,7 @@ CODE_QUALITY_FILE="code_quality.csv"
 # Linter results
 CODE_Q=$($LINTER_COMMAND | grep -oP "(?<=Your code has been rated at).*?(?=/)")
 echo "Code quality: $CODE_Q"
-echo "\"Code Quality\"" > $CODE_QUALITY_FILE
-echo $CODE_Q > $CODE_QUALITY_FILE
-echo $CODE_Q > $CODE_QUALITY_FILE
-echo $CODE_Q > $CODE_QUALITY_FILE
+echo $CODE_Q >> $CODE_QUALITY_FILE
+echo $CODE_Q >> $CODE_QUALITY_FILE
+echo $CODE_Q >> $CODE_QUALITY_FILE
 
