f2c2ec1726a53d43f7c4bca3045f77a4eec6d7c9
Yury D <SoulGate@yandex.ru>
2019-03-14 00:51:27 +0300

    Welcome to IT, guys!

--- quality_measurements.sh
+++ quality_measurements.sh
@@ -7,13 +7,14 @@ LINTER_COMMAND="pylint ytm_wd ytm_pageparser.py"
 CODE_QUALITY_FILE="code_quality.csv"
 DOCKER_SIZE_FILE="docker_image_size.csv"
 
+echo "Current user: $USER"
 # Linter results
 CODE_Q=$($LINTER_COMMAND | grep -oP "(?<=Your code has been rated at).*?(?=/)")
 echo "Code quality: $CODE_Q"
 echo $CODE_Q >> $CODE_QUALITY_FILE
 
 # Docker image size
-IMG_SIZE=$(docker image inspect owlsoul/ytmonitor --format='{{.Size}}')
+IMG_SIZE=`docker image inspect owlsoul/ytmonitor --format='{{.Size}}'`
 IMG_SIZE=$[$IMG_SIZE/1024/1024]
 echo "Docker image size: $IMG_SIZE MB"
 echo $IMG_SIZE >> $DOCKER_SIZE_FILE
