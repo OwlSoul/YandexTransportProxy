853ca9b5d7057b78f49b2a90f61c20a0d7cb448b
Yury D <SoulGate@yandex.ru>
2019-03-16 23:20:07 +0300

    Logger is now 10/10.

--- Logger.py
+++ Logger.py
@@ -1,12 +1,49 @@
+"""
+This is a logger module, to simplify logging to the screen.
+Logs messages are printed in the following format:
+
+   LOG LEVEL : [ Timestamp ] : Message
+
+Log levels include NONE, ERROR, WARNING, INFO and DEBUG.
+No log messages above current verbose level will be printed.
+
+"""
+# pylint: disable=C0103
+#  Disabling pylint snakeCase check for module name.
+# pylint: enable=C0103
+
+
 import sys
 import datetime
 
 class Logger:
+    """
+    Logger class, handles printing log messages based on set verbosity level.
+    """
     # Logging levels
+    NONE = 0
     ERROR = 1
     WARNING = 2
     INFO = 3
     DEBUG = 4
+
+    _verbose = ERROR
+    @property
+    def verbose(self):
+        """Current verbosity level of the program"""
+        return self._verbose
+
+    @verbose.setter
+    def verbose(self, value):
+        """Set current verbosity level of the program
+        :param value: desired verbosity level: NONE, ERROR, WARNING, INFO, DEBUG
+        :return: nothing
+        """
+        if value > self.DEBUG:
+            value = self.DEBUG
+        if value < self.NONE:
+            value = self.NONE
+        self._verbose = value
 
     def __init__(self, log_level):
         self.verbose = log_level
@@ -40,13 +77,33 @@ class Logger:
                 return
 
     def error(self, text):
+        """
+        Log error message
+        :param text: text to print
+        :return: nothing
+        """
         self.log(self.ERROR, text)
 
     def warning(self, text):
+        """
+                Log warning message
+                :param text: text to print
+                :return: nothing
+                """
         self.log(self.WARNING, text)
 
     def info(self, text):
+        """
+                Log info message
+                :param text: text to print
+                :return: nothing
+                """
         self.log(self.INFO, text)
 
     def debug(self, text):
+        """
+                Log debug message
+                :param text: text to print
+                :return: nothing
+                """
         self.log(self.DEBUG, text)
