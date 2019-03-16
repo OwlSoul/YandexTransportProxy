f54e14fb914517e05aa9433de18c1417521e170a
Yury D <SoulGate@yandex.ru>
2019-03-16 14:51:54 +0300

    Changes to "YandexTransportCore" logic.    - _getYandexJSON now returns array of JSON data.      This basically enables usage getAllInfo method for everything,      like getRouteInfo and getVehiclesInfo will be passed alltogether.

--- YandexTransportCore.py
+++ YandexTransportCore.py
@@ -52,11 +52,14 @@ class YandexTransportCore:
         """
         Universal method to get Yandex JSON results.
         :param url: initial url, get it by clicking on the route or stop
-        :param api_method: basically a string to find, like "maps/api/masstransit/getStopInfo"
-        :return: huge json data, error code
+        :param api_method: tuple of strings to find,
+               like ("maps/api/masstransit/getRouteInfo","maps/api/masstransit/getVehiclesInfo")
+        :return: array of huge json data, error code
         """
+        result_list = []
+
         if self.driver is None:
-            return None, self.RESULT_WEBDRIVER_NOT_RUNNING
+            return result_list, self.RESULT_WEBDRIVER_NOT_RUNNING
         self.driver.get(url)
 
         # Script to get Network data from Developer tools, huge thanks to this link:
@@ -73,11 +76,10 @@ class YandexTransportCore:
             network_data = json.loads(network_json, encoding='utf-8')
         except Exception as e:
             print(e)
-            return self.RESULT_NETWORK_PARSE_ERROR, None
+            return result_list, self.RESULT_NETWORK_PARSE_ERROR
 
-        # Getting the last executed API query
-        last_query = None
         url_reached = False
+        last_query = []
 
         self.network_queries_count = 0
         for entry in network_data:
@@ -87,31 +89,50 @@ class YandexTransportCore:
                     url_reached = True
                     continue
             else:
-                res = re.match(".*" + api_method + ".*", str(entry['name']))
-                if res is not None:
-                    last_query = entry['name']
+                for method in api_method:
+                    res = re.match(".*" + method + ".*", str(entry['name']))
+                    if res is not None:
+                        last_query.append({"url": entry['name'], "method": method})
+
+        #for query in last_query:
+        #    print(query)
 
         # Getting last API query results from cache by executing it again in the browser
-        if last_query != None:
-            self.driver.get(last_query)
+        if len(last_query) > 0:
+            for query in last_query:
+                self.driver.get(query['url'])
+
+                # Writing getStopInfo results to memory
+                output_stream = io.StringIO()
+                output_stream.write(self.driver.page_source)
+                output_stream.seek(0)
+
+                # Getting getStopInfo results to JSON
+                soup = BeautifulSoup(output_stream, 'lxml', from_encoding='utf-8')
+                body = soup.find('body')
+                if body is not None:
+                    body_string = body.string.encode('utf-8')
+                    try:
+                        returned_json = json.loads(body_string, encoding='utf-8')
+                        data = {"url": query['url'],
+                                "method": query['method'],
+                                "error": "OK",
+                                "data": returned_json}
+                    except Exception as e:
+                        data = {"url": query['url'],
+                                "method": query['method'],
+                                "error": "Failed to parse JSON"}
+                else:
+                    data={"url": query['url'],
+                          "method": query['method'],
+                          "error": "Failed to parse body of the response"}
+
+                result_list.append(data)
+
         else:
-            return None, self.RESULT_NO_LAST_QUERY
+            return result_list, self.RESULT_NO_LAST_QUERY
 
-        # Writing getStopInfo results to memory
-        output_stream = io.StringIO()
-        output_stream.write(self.driver.page_source)
-        output_stream.seek(0)
-
-        # Getting getStopInfo results to JSON
-        soup = BeautifulSoup(output_stream, 'lxml', from_encoding='utf-8')
-        body = soup.find('body').string.encode('utf-8')
-        try:
-            result_json = json.loads(body, encoding='utf-8')
-        except Exception as e:
-            print(e)
-            return self.RESULT_JSON_PARSE_ERROR, None
-
-        return result_json, self.RESULT_OK
+        return result_list, self.RESULT_OK
 
     # ----                                   SHORTCUTS TO USED APIs                                               ---- #
 
@@ -119,25 +140,35 @@ class YandexTransportCore:
         """
         Getting Yandex masstransit getStopInfo JSON results
         :param url: url of the stop (the URL you get when you click on the stop in the browser)
-        :return: huge json data, error code
+        :return: array of huge json data, error code
         """
-        return self._getYandexJSON(url, api_method="maps/api/masstransit/getStopInfo")
+        return self._getYandexJSON(url, api_method=("maps/api/masstransit/getStopInfo",))
 
     def getVehiclesInfo(self, url):
         """
         Getting Yandex masstransit getVehiclesInfo JSON results
         :param url: url of the stop (the URL you get when you click on the stop in the browser)
-        :return: huge json data, error code
+        :return: array of huge json data, error code
         """
-        return self._getYandexJSON(url, api_method="maps/api/masstransit/getVehiclesInfo")
+        return self._getYandexJSON(url, api_method=("maps/api/masstransit/getVehiclesInfo",))
 
     def getRouteInfo(self, url):
         """
         Getting Yandex masstransit getRouteInfo JSON results
+        :param url: url of the stop (the URL you get when you click on the stop in the browser)
+        :return: array of huge json data, error code
+        """
+        return self._getYandexJSON(url, api_method=("maps/api/masstransit/getRouteInfo",))
+
+    def getAllInfo(self, url):
+        """
+        Getting basically all Yandex Masstransit API JSON results related to requested URL
         :param url:
         :return:
         """
-        return self._getYandexJSON(url, api_method="maps/api/masstransit/getRouteInfo")
+        return self._getYandexJSON(url, api_method=("maps/api/masstransit/getRouteInfo",
+                                                    "maps/api/masstransit/getStopInfo",
+                                                    "maps/api/masstransit/getVehiclesInfo"))
 
 
 if __name__ == '__main__':
