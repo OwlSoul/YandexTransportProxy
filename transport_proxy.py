e458278c81ef856812135430baac690ab8ffaef9
Owl Automaton <OwlAutomaton@yandex.ru>
2019-03-29 18:50:40 +0300

    Release: 0.0.11-alpha

--- transport_proxy.py
+++ transport_proxy.py
@@ -15,7 +15,7 @@ Chromium browser.
 __author__ = "Yury D."
 __credits__ = ["Yury D.", "Pavel Lutskov", "Yury Alexeev"]
 __license__ = "MIT"
-__version__ = "0.0.10-alpha"
+__version__ = "0.0.11-alpha"
 __maintainer__ = "Yury D."
 __email__ = "SoulGate@yandex.ru"
 __status__ = "Alpha"
