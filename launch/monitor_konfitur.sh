f58de5b0bb0f455cac9e641d8f970c6bd80f64e8
Yury D <SoulGate@yandex.ru>
2019-04-02 14:00:35 +0300

    Some project structure beautifications.   - Removed stray folders (launch, plots)   - Moved "api-protocol.txt" to docs.

--- launch/monitor_konfitur.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 ../ytm_wd \
---verbose 4 \
---url "https://yandex.ru/maps/214/dolgoprudniy/?ll=37.490358%2C55.930217&masstransit%5BstopId%5D=stop__9898970&mode=stop&z=17" \
---chrome_driver_location "/usr/lib/chromium-browser/chromedriver" \
---wait_time 60 \
---run_once \
--- launch/monitor_okruzhnaya.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python3 ../ytm_wd \
---verbose 4 \
---out_mode "csv" \
---url "https://yandex.ru/maps/213/moscow/?ll=37.573801%2C55.848008&masstransit%5BstopId%5D=stop__10182565&mode=stop&z=18" \
---chrome_driver_location "/usr/lib/chromium-browser/chromedriver" \
---wait_time 60 \
---run_once \
